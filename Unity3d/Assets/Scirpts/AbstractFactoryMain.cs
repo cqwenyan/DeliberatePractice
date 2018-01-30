564c59540da4d215be5586476b66161ed0033e80
WenYan <253364208@qq.com>
2018-01-30 17:30:49 +0800

    state, abstractFactory, Observer, Builder,Facade and Template

---
+++ Unity3d/Assets/Scirpts/AbstractFactoryMain.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AbstractFactory
+{
+    public class AbstractFactoryMain : MonoBehaviour
+    {
+
+        void Start()
+        {
+            IRaceFactory raceFactory = new DwarfFactory();
+            FrostMagic 冰系矮人 = raceFactory.创建冰系角色();
+            冰系矮人.Atk();
+        }
+
+    }
+
+    interface IMagicOption
+    {
+
+    }
+
+    abstract class FireMagic : IMagicOption
+    {
+        public abstract void Atk();
+    }
+
+    abstract class FrostMagic : IMagicOption
+    {
+        public abstract void Atk();
+    }
+
+    interface IRaceFactory
+    {
+        FireMagic 创建火系角色();
+        FrostMagic 创建冰系角色();
+    }
+
+    class FireDwarf : FireMagic
+    {
+        public override void Atk()
+        {
+            Debug.Log("矮人踢出了有古典气息的一脚");
+        }
+    }
+
+    class FireOrcish : FireMagic
+    {
+        public override void Atk()
+        {
+            Debug.Log("兽人踢出了意味深长的一脚");
+        }
+    }
+
+    class FrostDwarf : FrostMagic
+    {
+        public override void Atk()
+        {
+            Debug.Log("矮人发起了冰冻攻击");
+        }
+    }
+
+    class FrostOrcish : FrostMagic
+    {
+        public override void Atk()
+        {
+            Debug.Log("兽人发起了冰冻攻击");
+        }
+    }
+
+    class DwarfFactory : IRaceFactory
+    {
+        public FireMagic 创建火系角色()
+        {
+            return new FireDwarf();
+        }
+
+        public FrostMagic 创建冰系角色()
+        {
+            return new FrostDwarf();
+        }
+    }
+
+    class OrcishFactory : IRaceFactory
+    {
+        public FireMagic 创建火系角色()
+        {
+            return new FireOrcish();
+        }
+
+        public FrostMagic 创建冰系角色()
+        {
+            return new FrostOrcish();
+        }
+    }
+}
---
+++ Unity3d/Assets/Scirpts/BuilderMain.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Builder
+{
+    public class BuilderMain : MonoBehaviour
+    {
+
+        void Start()
+        {
+            Director director = new Director();
+            TankBuilder apocalypseTankBuilder = new ApocalypseTankBuilder();
+            director.Construct(apocalypseTankBuilder);
+            Tank tank = apocalypseTankBuilder.生产坦克();
+            tank.ShowTank();
+        }
+
+    }
+
+    class Tank
+    {
+        IList<string> 部件List = new List<string>();
+
+        public void Add(string 部件)
+        {
+            部件List.Add(部件);
+        }
+
+        public void ShowTank()
+        {
+            foreach (var item in 部件List)
+                Debug.Log(item);
+        }
+    }
+
+
+    abstract class TankBuilder
+    {
+        public abstract void 装甲();
+        public abstract void 炮管();
+        public abstract void 引擎();
+        public abstract Tank 生产坦克();
+    }
+
+    class MirageTankBuilder : TankBuilder
+    {
+        Tank tank = new Tank();
+
+        public override void 引擎()
+        {
+            tank.Add("V6引擎");
+        }
+
+        public override void 炮管()
+        {
+            tank.Add("120mm炮管");
+        }
+
+        public override void 装甲()
+        {
+            tank.Add("拟态装甲");
+        }
+
+        public override Tank 生产坦克()
+        {
+            return tank;
+        }
+    }
+
+    class ApocalypseTankBuilder : TankBuilder
+    {
+        Tank tank = new Tank();
+
+        public override void 引擎()
+        {
+            tank.Add("V12引擎");
+        }
+
+        public override void 炮管()
+        {
+            tank.Add("200mm炮管");
+        }
+
+        public override void 装甲()
+        {
+            tank.Add("镂空的铁板");
+        }
+
+        public override Tank 生产坦克()
+        {
+            return tank;
+        }
+    }
+
+    class Director
+    {
+        public void Construct(TankBuilder tankBuilder)
+        {
+            tankBuilder.引擎();
+            tankBuilder.炮管();
+            tankBuilder.装甲();
+        }
+    }
+}
